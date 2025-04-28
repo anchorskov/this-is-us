e5cc7ef2df9cdbc589a4f8c1c12d81638921b5c6
Jim Skovgard <anchorskov@rmail.com>
2025-04-28 08:50:26 -0600

    fix(auth): expose firebaseConfig on window and use it in init partial

--- static/js/firebase-config.js
+++ static/js/firebase-config.js
@@ -1,10 +1,10 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyB2JqSDeOgNOdMHCfHqaC78Rgr-l7LqIkU",
-    authDomain: "this-is-us-events.firebaseapp.com",
-    projectId: "this-is-us-events",
-    storageBucket: "this-is-us-events.firebasestorage.app",
-    messagingSenderId: "215038360222",
-    appId: "1:215038360222:web:98677c77158d282c9ad98f",
-    measurementId: "G-DKHTH767TD"
-  };
+// Firebase project configuration
+window.firebaseConfig = {
+  apiKey: "AIzaSyB2JqSDeOgNOdMHCfHqaC78Rgr-l7LqIkU",
+  authDomain: "this-is-us-events.firebaseapp.com",
+  projectId: "this-is-us-events",
+  storageBucket: "this-is-us-events.firebasestorage.app",
+  messagingSenderId: "215038360222",
+  appId: "1:215038360222:web:98677c77158d282c9ad98f",
+  measurementId: "G-DKHTH767TD"
+};
