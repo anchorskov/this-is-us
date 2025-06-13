d787752943a3636987fd2b6a8cfd9fd8c4d54961
Jim Skovgard <anchorskov@rmail.com>
2025-06-13 08:47:05 -0600

    fixed 4 townhall

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
-    // add other PostCSS plugins here if you need them later
+    tailwindcss: {}, // Add Tailwind CSS as a PostCSS plugin
+    autoprefixer: {},
+    // Add other PostCSS plugins here if you need them later
   }
 };
